9b1b9ea70a64a539a4f246d2ded6c826940d5e06
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-04-26 22:48:28 +0100

    Upgrade to QUnit2

--- test/qunit.js
+++ test/qunit.js
@@ -5,20 +5,12 @@ var isQunit = testType === 'qunit';
 
 if (isMochaQUnitUI) {
 	// mocha-qunit-ui does not support async
-	QUnit.assert.async = function () {
-		QUnit.stop();
-		return function done (error) {
-			if (error) {
-				return QUnit.ok(false, '' + error);
-			}
-			QUnit.start();
-		};
-	};
+	QUnit.assert.async = QUnit.async;
 
 	QUnit.test = test;
 	module.exports =  QUnit;
 } else if (isQunit) {
-	module.exports = require('qunitjs');
+	module.exports = require('qunit');
 } else {
 	module.exports = require('steal-qunit');
 }
